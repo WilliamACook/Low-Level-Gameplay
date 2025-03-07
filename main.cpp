7acdc1b230be2b8a1b36f7961f4e13064c95c03f
William Cook <115143803+WilliamACook@users.noreply.github.com>
2025-03-07 17:57:25 +0000

    Moving Shape

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 /*
     This version of the SFML "hello world" is statically linked, you may wish to try the dynamically linked version as well.
@@ -6,8 +7,19 @@
 int WinMain()
 {
     sf::RenderWindow window(sf::VideoMode({ 400, 400 }), "SFML works!");
-    sf::CircleShape shape(100.f);
+    sf::CircleShape shape(10.f);
     shape.setFillColor(sf::Color::Green);
+
+    sf::Time lastTime = sf::Time();
+    sf::Time currentTime = sf::Time();
+    int distPerSecond = 500;
+    int timeStep = 10;  // 10 ms, 100 updates per second
+
+    sf::Clock timer = sf::Clock ();
+    timer.start();
+    lastTime = timer.getElapsedTime();
+
+    sf::Clock clock;
 
     while (window.isOpen())
     {
@@ -17,10 +29,50 @@ int WinMain()
         {
             if (event->is<sf::Event::Closed>())
                 window.close();
+
+            else if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>())
+            {
+                if (keyPressed->scancode == sf::Keyboard::Scancode::Escape)
+                    window.close();
+            }
+
         }
 
+        currentTime = timer.getElapsedTime();
+        int timeDelta = (currentTime.asMilliseconds() - lastTime.asMilliseconds());
+        if (timeDelta > timeStep)
+        {
+            lastTime = currentTime;
+            float shiftDistance = distPerSecond * timeDelta / 1000;
+            //Controls
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left))
+            {
+                // left key is pressed: move our character
+                shape.move({ -shiftDistance, 0.f });
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right))
+            {
+                // left key is pressed: move our character
+                shape.move({ 1.f, 0.f });
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up))
+            {
+                // left key is pressed: move our character
+                shape.move({ 0.f, -1.f });
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down))
+            {
+                // left key is pressed: move our character
+                shape.move({ 0.f, 1.f });
+            }
+        }
         window.clear();
         window.draw(shape);
         window.display();
+
+        //sf::Time elapsed = clock.restart();
+
     }
+
+    
 }
