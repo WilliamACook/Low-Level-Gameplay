87493b932f949f8b9b11b94ab9791bf411a65c01
William Cook <115143803+WilliamACook@users.noreply.github.com>
2025-03-11 17:56:30 +0000

    Added Texture and wrap screen

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <SFML/Graphics.hpp>
+#include <chrono>
 
 /*
     This version of the SFML "hello world" is statically linked, you may wish to try the dynamically linked version as well.
@@ -8,20 +9,31 @@ int WinMain()
     sf::RenderWindow window(sf::VideoMode({ 400, 400 }), "SFML works!");
     sf::CircleShape shape(10.f);
     shape.setFillColor(sf::Color::Green);
-
-    sf::Time lastTime = sf::Time();
-    sf::Time currentTime = sf::Time();
+    
     int distPerSecond = 100;
     int timeStep = 10;  // 10 ms, 100 updates per second
-	float gravity = 10.f;
 	float groundY = 350.f;
-	sf::Vector2f velocity(0.f, 0.f);
 
-    sf::Clock timer = sf::Clock ();
-    timer.start();
-    lastTime = timer.getElapsedTime();
+	//CharacterTexture
+	const sf::Image character("assets/character.png");
+	sf::Texture characterText;
+	bool result = characterText.loadFromImage(character, false, sf::IntRect({ 0,0 }, { 24,24 }));
+	sf::Sprite spritey(characterText);
+	//spritey.scale(sf::Vector2(2.0f, 2.0f));
 
-    sf::Clock clock;
+	//EnemyTexture
+	const sf::Image enemy("assets/enemy.png");
+	sf::Texture enemyText;
+	bool enemyresult = enemyText.loadFromImage(enemy, false, sf::IntRect({ 0,0 }, { 24,24 }));
+	sf::Sprite sp_enemy(enemyText);
+
+	sf::Time lastTime = sf::Time();
+	sf::Time currentTime = sf::Time();
+	sf::Clock timer = sf::Clock();
+	timer.start();
+	lastTime = timer.getElapsedTime();
+
+	bool transitioned = false;
 
     while (window.isOpen())
     {
@@ -39,11 +51,6 @@ int WinMain()
             }
 
         }
-		float deltaTime = clock.restart().asSeconds();
-		velocity.y += gravity;
-		shape.move(velocity * deltaTime);
-		if(velocity.y > 5.f)
-			velocity.y = 5.f;
 
         currentTime = timer.getElapsedTime();
         int timeDelta = (currentTime.asMilliseconds() - lastTime.asMilliseconds());
@@ -52,20 +59,59 @@ int WinMain()
             lastTime = currentTime;
             float shiftDistance = distPerSecond * timeDelta / 1000;
             //Controls
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left))
-				shape.move({ -shiftDistance, 0.f });
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
+				//spritey.move({ -shiftDistance, 0.f });
 
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right))
-				shape.move({ shiftDistance, 0.f });
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up))
-				shape.move({ 0.f, -shiftDistance });
+			{
+				float x = spritey.getPosition().x - shiftDistance;
+				float y = spritey.getPosition().y;
+				if (x <  .0f) { x = 400.0f + x; }
+				spritey.setPosition(sf::Vector2f{ x,y });
+			}
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
+				spritey.move({ shiftDistance, 0.f });
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
+				spritey.move({ 0.f, -shiftDistance });
 
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down))
-				shape.move({ 0.f, shiftDistance });
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
+				spritey.move({ 0.f, shiftDistance });
+
+			//Gravity
+			spritey.move({ 0.f, 0.1f });
+			
+			//Wrap screen
+			if (sp_enemy.getPosition().x > 400.f)
+			{			
+				sp_enemy.setPosition({-10, 0});
+			}
+			else
+			{
+				sp_enemy.move({ 1.f, 0.f });
+			}
+
+			/*if (spritey.getPosition().x > 400.f && transitioned == false)
+			{
+				transitioned = true;
+				spritey.setPosition({ -10,spritey.getPosition().y });
+			}
+			if (spritey.getPosition().x <  0.f && transitioned == false)
+			{
+				transitioned = true;
+				spritey.setPosition({ 400.f,spritey.getPosition().y });
+			}
+
+			if (spritey.getPosition().x > 1 && spritey.getPosition().x < 399 && transitioned == true)
+			{
+				transitioned = false;
+			}*/
+
+				
         }
 
         window.clear();
-        window.draw(shape);
+        //window.draw(shape);
+		window.draw(spritey);
+		window.draw(sp_enemy);
         window.display();
 
     }
